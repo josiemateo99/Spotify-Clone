06cae770eb8bd29bdf9e7d78274309506b2079bd
josiemateo99 <josiemateo99@gmail.com>
2022-07-25 19:55:28 -0400

    Log In Page Failure

--- spotify-clone/middleware.js
+++ spotify-clone/middleware.js
@@ -1,24 +1,26 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from 'next/server';
-import login from "./pages/login";
+import { getToken } from 'next-auth/jwt'
+import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
 
-    //Token will exist if the user is logged in
-    const token = await getToken({req, secret: process.env.JWT_SECRET});
-    const {pathname} = req.nextUrl;
-
-    //Allow the request if the following is true..
-        // 1. If the Token Exist
-    if (pathname.includes('/api/auth') || token){
-        return NextResponse.next();
-    }
+    // token will exist if the user is logged in
+    const token = await getToken({ req, secret: process.env.JWT_SECRET })
     
-    if(!token && pathname !== '/login'){
+    const { pathname } = req.nextUrl
 
-        const loginPage = '../login'
-        return NextResponse.redirect(new URL(loginPage, req.url))
+    const url = req.nextUrl.clone()
+    url.pathname = '/login'
+  
+    // ALlow the request if the following is true:
+    // 1) it's a request to next-auth session
+    // 2) the token exists
 
+    if (pathname.includes('/api/auth') || token) {
+        return NextResponse.next()
     }
 
+    // redirect to login page if the user is not logged in and they are requesting a protected routeing a protected route
+    if (!token && pathname !== '/login') {
+        return NextResponse.redirect(url)
+    }
 }
--- spotify-clone/pages/login.js
+++ spotify-clone/pages/login.js
@@ -10,8 +10,7 @@ function login({providers}) {
           
           <div key={provider.name}>
             <button className="bg-[#18D860] text-white p-5 rounded-full"
-            onClick={() => signIn(provider.id, {callbackUrl: "/"})}
-            > 
+            onClick={() => signIn(provider.id, {callbackUrl: "/"})}> 
               Login with {provider.name}
             </button>
           </div>
