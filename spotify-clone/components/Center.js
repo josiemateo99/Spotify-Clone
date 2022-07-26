ba57a403eb5923c024359bd0f557649c6dd4ce54
josiemateo99 <josiemateo99@gmail.com>
2022-07-26 18:15:22 -0400

    Added Center Color Gradient

--- spotify-clone/components/Center.js
+++ spotify-clone/components/Center.js
@@ -1,9 +1,26 @@
 import { ChevronDoubleDownIcon } from "@heroicons/react/outline";
 import { useSession } from "next-auth/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { shuffle } from "lodash";
+
+const colors = [
+    "from-indigo-500",
+    "from-blue-500",
+    "from-red-500",
+    "from-yellow-500",
+    "from-pink-500",
+    "from-purple-500",
+];
+
 
 function Center() {
     const {data: session} = useSession()
+    const [color,setColor] = useState(null)
+
+    useEffect(() => {
+        setColor(shuffle(colors).pop())
+    }, [])
+
   return(
     <div className="flex-grow">
         <header className="absolute top-5 right-8">
@@ -18,7 +35,7 @@ function Center() {
         </header>
 
         <section 
-        className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white padding-8`}>
+        className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white padding-8`}>
         <h1>
             Hello World!
         </h1>
