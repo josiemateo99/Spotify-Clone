8f106b298555df3c7e5667aa49c3cd450dc4bc09
josiemateo99 <josiemateo99@gmail.com>
2022-07-26 17:59:55 -0400

    Center Page Added

--- spotify-clone/pages/index.js
+++ spotify-clone/pages/index.js
@@ -1,14 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Sidebar from '../components/Sidebar'
+import Center from '../components/Center'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
-      <main className=''>
+      <main className="flex">
         <Sidebar/>
-        {/* Center */}
+        <Center/>
       </main>
 
       <div>
