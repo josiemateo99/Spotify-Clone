b41f61102010a2c251a99774f32d01ed88df565a
josiemateo99 <josiemateo99@gmail.com>
2022-08-17 14:07:47 -0400

    Added Playlist Covers to Center

--- spotify-clone/components/Center.js
+++ spotify-clone/components/Center.js
@@ -39,7 +39,7 @@ function Center() {
     console.log(playlist)
   return(
     //
-    <div className="flex-grow">
+    <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
         <header className="absolute top-5 right-8">
             
             <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full
--- spotify-clone/components/Songs.js
+++ spotify-clone/components/Songs.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { useRecoilValue } from "recoil";
 import { playlistState } from "../atoms/playlistAtom";
+import Song from "./Song";
 
 function Songs() {
   const playlist = useRecoilValue(playlistState); 
 
   return (
-  <div className="text-white">
-    {playlist?.tracks.items.map((track) => (
-      <div>{track.track.name} </div>
+  <div className="px-8 flex flex-col text-white">
+    {playlist?.tracks.items.map((track, i) => (
+      <Song key={track.id} track={track} order={i}/>
     ))}
 
   </div>
