26a63bd110f487db2efa24a48350496f66f838ef
josiemateo99 <josiemateo99@gmail.com>
2022-08-22 18:58:30 -0400

    Removed Log Out Button

--- spotify-clone/components/Song.js
+++ spotify-clone/components/Song.js
@@ -6,7 +6,7 @@ import { millisToMinutesAndSeconds } from "../lib/time";
 function Song({order, track}) {
     const spotifyApi = useSpotify();
 
-    return <div className="grid grid-cols-2 text-gray-500">
+    return <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
         <div className="flex items-center space-x-4">
             <p>{order+1}</p>
             <img 
@@ -15,12 +15,12 @@ function Song({order, track}) {
 
             <div>
                 <p className="w-36 lg:w-64 text-white truncate ">{track.track.name}</p>
-                <p>{track.track.artists[0].name}</p>
+                <p className="w-40">{track.track.artists[0].name}</p>
             </div>
         </div>
 
         <div className="flex items-center justify-between ml-auto md:ml-0">
-            <p className="hidden md:inline">{track.track.album.name}</p>
+            <p className="w-40 hidden md:inline">{track.track.album.name}</p>
             <p>{millisToMinutesAndSeconds(track.track.duration_ms)}</p>
         </div>
     </div>;
