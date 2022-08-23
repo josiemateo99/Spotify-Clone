f06a0701c328c6a717f4011a5ee865064dff9027
josiemateo99 <josiemateo99@gmail.com>
2022-08-22 20:09:09 -0400

    config logout button

--- spotify-clone/components/Center.js
+++ spotify-clone/components/Center.js
@@ -1,5 +1,5 @@
 import { ChevronDoubleDownIcon } from "@heroicons/react/outline";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react";
 import { shuffle } from "lodash";
 import { useRecoilState, useRecoilValue } from "recoil";
@@ -43,7 +43,7 @@ function Center() {
         <header className="absolute top-5 right-8">
             
             <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full
-            padding-1 pr-2 text-white p-1">
+            padding-1 p-1 pr-2 text-white" onClick={signOut}>
                 <img className="rounded-full w-10 h-10" 
                 src={session?.user.image} alt="" />
 
