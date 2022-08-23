285206675a0f94437b9f2623a35baac8a68dba4b
josiemateo99 <josiemateo99@gmail.com>
2022-08-22 21:42:47 -0400

    Config Player Component

--- spotify-clone/components/Player.js
+++ spotify-clone/components/Player.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import useSpotify from "../hooks/useSpotify";
+import { useSession } from "next-auth/react";
+import { useRecoilState } from "recoil";
+import { isPlayingState } from "../atoms/songAtom";
 
 function Player() {
+    const spotifyApi = useSpotify();
+    const {data: session, staus} = useSession();
+    const [currentTrackId, setCurrentIdTrack] = useRecoilState(isPlayingState);
+    const [volume, setVolume] = useState(50);
+    const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
   return (
         <div>
-            <h1>Player</h1>
+            {/* Left */}
+            <div>
+                <img src=""></img>
+            </div>
         </div>
   )
 }
