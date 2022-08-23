ac56d627cf955894f8841928210bca6ab115176c
josiemateo99 <josiemateo99@gmail.com>
2022-08-23 17:27:11 -0400

    Added Play Bar Photo/Name/Artist

--- spotify-clone/components/Player.js
+++ spotify-clone/components/Player.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useSpotify from "../hooks/useSpotify";
 import { useSession } from "next-auth/react";
 import { useRecoilState } from "recoil";
 import { isPlayingState } from "../atoms/songAtom";
+import useSongInfo from "../hooks/useSongInfo";
 
 function Player() {
     const spotifyApi = useSpotify();
@@ -10,11 +11,43 @@ function Player() {
     const [currentTrackId, setCurrentIdTrack] = useRecoilState(isPlayingState);
     const [volume, setVolume] = useState(50);
     const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+
+    const songInfo = useSongInfo()
+
+    const fetchCurrentSong = () => {
+        if (!songInfo) {
+            spotifyApi.getMyCurrentPlayingTrack().then((data) => {
+                setCurrentIdTrack(data.body?.item?.id)
+
+                spotifyApi.getMyCurrentPlaybackState().then((data) =>{
+                    console.log("Now Playing: ", data.body);
+                    setIsPlaying(data.body?.is_playing);
+                })
+            })
+        }
+    }
+
+    useEffect(() =>{
+        if (spotifyApi.getAccessToken() && !currentTrackId){
+            fetchCurrentSong();
+            setVolume(50);
+        }
+    }, [currentTrackId,spotifyApi,session])
+
   return (
-        <div>
+        <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid-cols-3 text-xs md:text-base px-2 md:px-8">
             {/* Left */}
+            <div className="flex items-center space-x-4">
+                <img 
+                className="hidden md:inline h-10 w-10" 
+                src={songInfo?.album.images?.[0]?.url}
+                alt = ""
+                />
+            </div>
+
             <div>
-                <img src=""></img>
+                <h3>{songInfo?.name}</h3>
+                <p>{songInfo?.artists?.[0]?.name}</p>
             </div>
         </div>
   )
--- spotify-clone/middleware.js
+++ spotify-clone/middleware.js
@@ -3,26 +3,21 @@ import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
 
+    const url = req.nextUrl.clone()
     // token will exist if the user is logged in
     const token = await getToken({ req, secret: process.env.JWT_SECRET })
-    
     const { pathname } = req.nextUrl
-
-    const url = req.nextUrl.clone()
-    url.pathname = '/login'
   
-    // ALlow the request if the following is true:
-    // 1) it's a request to next-auth session
-    // 2) the token exists
+    if (pathname.startsWith('/_next') || pathname.includes('.')){
 
-    if (pathname.includes('/api/auth') || token) {
-        console.log("api/auth and token")
-        return NextResponse.next()
     }
 
-    //redirect to login page if the user is not logged in and they are requesting a protected routeing a protected route
-    // if (!token && pathname !== '/login') {
-    //     console.log("Log In Page")
-    //     return NextResponse.redirect(url)
-    // }
+    if(pathname.includes("/api/auth") || token){
+        return NextResponse.next();
+    }
+
+//     if(!token && pathname!=="/login"){
+//         url.pathname = "/login";
+//         return NextResponse.redirect(url);
+//     }
 }
